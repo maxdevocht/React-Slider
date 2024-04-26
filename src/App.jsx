1c66f2ac43ab29ce96d40612fa2cb52bf40e59f7
Max de Vocht <maxdevocht@hotmail.com>
2024-04-26 11:57:18 +0200

    Added images

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Slider from './components/Slider'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Slider />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Slider.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+
+export default function Slider() {
+    return (
+        <Splide className="splide" hasTrack={false}
+            options={ {
+                rewind: true,
+                gap   : '1rem',
+            } }
+            aria-label="My Favorite Images"
+            >
+                <SplideTrack className="slider">
+                    <SplideSlide>
+                        <h1>Title 1</h1>
+                        <img src="/1.svg" alt="Image 1"/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <h1>Title 2</h1>
+                        <img src="/2.svg" alt="Image 2"/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <h1>Title 3</h1>
+                        <img src="/3.svg" alt="Image 3"/>
+                    </SplideSlide>
+                </SplideTrack>
+
+        </Splide>
+    )
+}
